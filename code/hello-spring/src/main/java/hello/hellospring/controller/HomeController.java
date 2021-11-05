e239973477756f359a3c656b7b5bb5233118a5a4
HoJun Yang <didghwns0514@gmail.com>
2021-11-05 16:01:16 +0900

    doc,build(.md/.java/home.html): Add;
    - Content; New welcompage with home
    1) initail index page는 static으로 template이 우선순위가 높아서 먼저
       로드됨

---
+++ code/hello-spring/src/main/java/hello/hellospring/controller/HomeController.java
@@ -0,0 +1,14 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/") // domain 완전 처음 -> template작성하면 그것 불러옴
+    public String home() {
+        return "home";
+    }
+
+}
