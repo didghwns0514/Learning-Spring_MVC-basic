ded5edd1d6638e17c3b9738d9bc105a45beb936c
HoJun Yang <didghwns0514@gmail.com>
2021-11-06 18:08:42 +0900

    doc,build(.md/.java): Add; Ch6-3
    - Content
    1) Thymleaf th: for loop
    2) How to show Content

--- code/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ code/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -6,8 +6,12 @@ import hello.hellospring.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller // @Bean @Component 마냥, Spring이 넣어두고 관리하게 되는 대상으로 처음 시작때 등록
 @RequiredArgsConstructor // private 붙은 것에 생성자 주입 자동으로 관리
@@ -24,6 +28,16 @@ public class MemberController {
     @GetMapping("/members/new")
     public String createForm() {
         return "members/createMembersForm";
+    }
+
+    @GetMapping("/members")
+    public String membersList(Model model) {
+
+        ArrayList<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+
+        return "members/searchMembersList";
+
     }
 
     @PostMapping("members/new") // Get으로 보여주고 나서 Form 받을 때 사용 Post 주의
